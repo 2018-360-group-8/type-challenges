36277c87baeb044e5b5a10a092110123abec521d
caohuilin <caohuilin@bytedance.com>
2022-08-12 18:50:37 +0800

    feat: easy result

--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,5 @@
-type MyAwaited<T> = any
+type MyAwaited<T extends Promise<any>> = T extends Promise<infer V>
+  ? V extends Promise<any>
+    ? MyAwaited<V>
+    : V
+  : T
--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,18 @@
-type Includes<T extends readonly any[], U> = any
+// type Equal<T, U> = T extends U ? U extends T ? true : false : false
+type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y
+  ? 1
+  : 2
+  ? true
+  : false
+
+type IterativeIncludes<T extends readonly any[], U> = {
+  [K in keyof T]: Equal<T[K], U>
+}[number] extends false
+  ? false
+  : true
+
+type Includes<T extends readonly any[], U> = T extends [infer F, ...infer R]
+  ? Equal<F, U> extends true
+    ? true
+    : IterativeIncludes<T, U>
+  : false
